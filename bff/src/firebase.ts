49835bfd632e0c6020ed9e5546befebd9d006d85
Maria Lemos <maria.lemos@b2wdigital.com>
2022-05-19 23:56:43 -0300

    chore: testando firebase

--- bff/src/firebase.ts
+++
@@ -1,9 +0,0 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// Initialize Firebase
